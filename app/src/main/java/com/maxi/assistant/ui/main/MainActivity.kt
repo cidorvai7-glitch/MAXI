5cda59a045d24f8f6ceb4dcc05d1a5b5a849bfe8
cidorvai7-glitch <cidorvai7@gmail.com>
2026-05-20 17:44:33 +0600

    Source code uploaded from Termux

---
+++ app/src/main/java/com/maxi/assistant/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maxi.assistant.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maxi.assistant.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
